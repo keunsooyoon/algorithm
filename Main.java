4e2ac3f31c3951be7d0513199c33e7c7f73a03fe
Austin <austiny@snu.ac.kr>
2023-01-20 05:11:52 +0000

    sss

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
 class Main {
   public static void main(String[] args) {
-	// 삼각형의 면적 구하기
-	
-	// base 4, height 2
-	
-	double base = 4;
-	double height = 2;
-
-	double area = base * height / 2;
-
-	System.out.println(area);
-	  
-	  
+    System.out.println("깃헙 업로드 테스트");
   }
 }
